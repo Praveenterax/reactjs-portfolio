e5ecf8e0386350c0f999577a424ebcd77e64901f
praveenterax <praveendunga111@gmail.com>
2026-02-07 19:56:50 +0530

    Removed Sanity token

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -6,7 +6,6 @@ export const client = createClient({
   dataset: "production",
   apiVersion: "2022-02-01",
   useCdn: true,
-  token: import.meta.env.VITE_SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
